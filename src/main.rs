2c4a0879c120786cca0b3546648356a744fefa31
Douglas Campos <qmx@qmx.me>
2018-02-25 03:46:34 +0000

    allow external control of the delay between samples

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[macro_use]
-extern crate nom;
-
+extern crate clap;
 extern crate failure;
+#[macro_use]
+extern crate nom;
 
 mod model;
 mod parser;
@@ -13,9 +14,27 @@ use std::{thread, time};
 
 use model::Stat;
 
+use clap::{App, Arg};
+
 fn main() {
+    let m = App::new(crate_name!())
+        .about(crate_description!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .arg(
+            Arg::with_name("delay")
+                .short("d")
+                .long("delay")
+                .value_name("DELAY")
+                .help("delay in miliseconds")
+                .default_value("100"),
+        )
+        .get_matches();
+
+    let delay = value_t!(m, "delay", u64).unwrap_or(100);
+
     let s1 = get_stat().unwrap();
-    let d = time::Duration::from_millis(100);
+    let d = time::Duration::from_millis(delay);
     thread::sleep(d);
     let s2 = get_stat().unwrap();
 
